6e780c12f21d0b146dc6ef1998cc46a840b28bf5
lizersca <lizersaiz@gmail.com>
2026-06-07 14:43:03 +0200

    feat(pom): add openapi plus swagger deps

---
+++ gateway-service/src/main/java/org/jtp/infra/rest/CreateOrdersRequest.java
@@ -0,0 +1,20 @@
+package org.jtp.infra.rest;
+//
+//import jakarta.validation.constraints.Min;
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.NotNull;
+//
+//public class CreateOrdersRequest {
+//
+//    @NotBlank
+//    String customerId,
+//
+//    @NotBlank
+//    String symbol,
+//
+//    @NotNull
+//    Side side,
+//
+//    @Min(1)
+//    int quantity
+//}
